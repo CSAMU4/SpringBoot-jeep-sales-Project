d6dafe70f9f71e0442798edef53b1213dcd65d1b
Candace Samuels <samuelsmcandace@gmail.com>
2023-04-12 23:00:25 -0400

    Week 14

---
+++ jeep-sales/src/main/java/com/promineotech/jeep/entity/Jeep.java
@@ -0,0 +1,18 @@
+package com.promineotech.jeep.entity;
+
+import java.math.BigDecimal;
+import lombok.Builder;
+import lombok.Data;
+
+@Data 
+@Builder 
+public class Jeep {
+  
+  private Long modelPK; 
+  private JeepModel modelID; 
+  private String trimLevel; 
+  private int numDoors; 
+  private int wheelSize; 
+  private BigDecimal basePrice;
+  
+  }  
---
+++ jeep-sales/src/main/java/com/promineotech/jeep/service/JeepSalesService.java
@@ -0,0 +1,13 @@
+package com.promineotech.jeep.service;
+
+import java.util.List;
+import com.promineotech.jeep.entity.Jeep;
+
+public interface JeepSalesService {
+
+  static List<Jeep> fetchJeeps(String model, String trim) {
+    
+    return null;
+  }
+
+}
